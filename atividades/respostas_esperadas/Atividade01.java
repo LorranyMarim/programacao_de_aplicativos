d0fc835aa47b87b327df92bf62aa0940ae233944
LorranyMarim <lorranybatistaamorim@gmail.com>
2025-09-15 16:54:24 -0300

    Segunda aula com .gitignore

---
+++ atividades/respostas_esperadas/Atividade01.java
@@ -0,0 +1,25 @@
+
+
+package com.mycompany.atividade01;
+
+/**
+ *
+ * @author lorrany.batista
+ */
+public class Atividade01 {
+
+    public static void main(String[] args) {
+      int A, B,C;
+      double D;
+      
+      A = 25;
+      B = A*3;
+      C = 10;
+      D = A/C;
+      
+      System.out.println(A);
+      System.out.println(B);
+      System.out.println(C);
+      System.out.println(D);
+    }
+}
